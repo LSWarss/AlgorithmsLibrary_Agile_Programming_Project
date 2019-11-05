62f8fcd20fbc7cbc85c3b11c650f055bd528b829
Jakub Popiol <popioljakub@gmail.com>
2019-11-05 09:38:00 +0100

    add mergesort

---
+++ src/com/company/MergeSort.java
@@ -0,0 +1,76 @@
+package mergesort;
+
+public class MergeSort {
+
+    public void merge(int[] arr, int l, int m, int r)
+    {
+        //find size of subbarrays to be merged
+        int sizeLeft = m - l + 1;
+        int sizeRight = r - m;
+        
+        //create temp subarrays
+        int[] tempLeft = new int[sizeLeft];
+        int[] tempRight = new int[sizeRight];
+        
+        //copy data to temp arrays
+        for (int i = 0; i < sizeLeft; ++i)
+        {
+            tempLeft[i] = arr[l + i];
+        }
+        for (int j = 0; j < sizeRight; ++j)
+        {
+            tempRight[j] = arr[m + 1 + j];
+        }
+        
+        /* merge */
+        int initialIndexLeft = 0;
+        int initialIndexRight = 0;
+        
+        int initialIndexMerged = l;
+        
+        while (initialIndexLeft < sizeLeft && initialIndexRight < sizeRight)
+        {
+            if (tempLeft[initialIndexLeft] <= tempRight[initialIndexRight])
+            {
+                arr[initialIndexMerged] = tempLeft[initialIndexLeft];
+                initialIndexLeft++;
+            }
+            else
+            {
+                arr[initialIndexMerged] = tempRight[initialIndexRight];
+                initialIndexRight++;
+            }
+            initialIndexMerged++;
+        }
+        
+        //copy remaining
+        while (initialIndexLeft < sizeLeft)
+        {
+            arr[initialIndexMerged] = tempLeft[initialIndexLeft];
+            initialIndexLeft++;
+            initialIndexMerged++;
+        }
+        while (initialIndexRight < sizeRight)
+        {
+            arr[initialIndexMerged] = tempRight[initialIndexRight];
+            initialIndexRight++;
+            initialIndexMerged++;
+        }
+    }
+    
+    public void sort(int[] arr, int l, int r)
+    {
+        if (l < r)
+        {
+            //middle point
+            int m = (l + r)/2;
+            
+            //sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m+1, r);
+            
+            //merge sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+}
