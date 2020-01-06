a53b890d0ac6d96366485837a85c07a5f91a7cbb
KrzempekP <56874994+KrzempekP@users.noreply.github.com>
2020-01-06 07:28:32 +0100

    Update InsertionSort.java

--- AgileProgrammingProject/src/main/java/sorting/InsertionSort.java
+++ AgileProgrammingProject/src/main/java/sorting/InsertionSort.java
@@ -1,17 +1,49 @@
 package sorting;
 
-class InsertionSort {
-    void sort(int arr[]) {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
+import node.Node;
 
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
+import java.util.ArrayList;
+import java.util.Arrays;
+import javafx.animation.Transition;
+import javafx.scene.paint.Color;
+import node.Node;
+
+public class InsertionSort extends AbstractSort {
+
+  private ArrayList<Transition> transitions;
+
+  public InsertionSort() {
+    this.transitions = new ArrayList<>();
+  }
+
+  @Override
+  public ArrayList<Transition> Sort(Node[] arr) {
+
+    for (int i = 1; i < arr.length; i++) {
+      int j = i - 1;
+      CNode key = arr[i];
+      int keyIndx = i;
+
+      transitions.add(colorCNode(arr, SELECT_COLOR, i));
+
+      while(j >= 0 && arr[j].getValue() > key.getValue()) {
+        pt.getChildren().add(arr[j].moveX(DX));
+        arr[j + 1] = arr[j];
+        j--;
+      }
+
+      arr[j + 1] = key;
+
+      pt.getChildren().add(key.moveX(DX * (j + 1 - i)));
+      transitions.add(pt);
+      transitions.add(colorCNode(arr, START_COLOR, j + 1));
+
     }
+
+    transitions.add(colorCNode(Arrays.asList(arr), SORTED_COLOR));
+
+    return transitions;
+
+  }
+  
 }
